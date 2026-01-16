32acdd0c1f0e5a47ef26753715dc6f45dd7fe1a9
Developer866 <opeyemijoseph866@gmail.com>
2026-01-16 21:17:42 +0100

    refactor: convert goal controller methods to use express-async-handler

--- backend/controllers/goalcontroller.js
+++ backend/controllers/goalcontroller.js
@@ -2,31 +2,34 @@ const expressHandler = require("express-async-handler");
 // desc Get goals
 // @route Get /api/goals
 // @access Private
-const getGoal = async(req, res) => {
+const getGoal = expressHandler(async(req, res) => {
   res.status(200).json({ message: "Get Goals" });
-};
+})
+
 // desc set goal
 // @route Set /api/goals
 // @access Private
-const setGoal = async(req, res) => {
+const setGoal =expressHandler( async(req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error("Please add a text field")
     }
   console.log(req.body);
   res.status(200).json({ message: "Set Goals" });
-};
+})
+
 // desc update goal
 // @route PUt /api/goals/:id
 // @access Private
-const updateGoal = async(req, res) => {
+const updateGoal = expressHandler( async(req, res) => {
   res.status(200).json({ message: `update Goals ${req.params.id}` });
-};
+})
+
 // desc Delete goals
 // @route Delete /api/goals/id
 // @access Private
-const deleteGoal = async(req, res) => {
+const deleteGoal = expressHandler(async(req, res) => {
   res.status(200).json({ message: `delete Goals ${req.params.id}` });
-};
+})
 
 module.exports = { getGoal, setGoal, updateGoal, deleteGoal };
