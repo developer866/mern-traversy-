c325f9d35922eccd1033bebd2e20bc17ec09e444
Developer866 <opeyemijoseph866@gmail.com>
2026-01-16 21:32:02 +0100

    feat: add mongoose connection and update package dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 const app = express();
+dotenv.config();
+
 const { errorHandler } = require('./middleware/errorMiddleware');
 
+const connectDB = require('./config/db');
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
