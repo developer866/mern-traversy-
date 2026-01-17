554573c475aa19869582d0ec257787bdc63523af
Developer866 <opeyemijoseph866@gmail.com>
2026-01-17 21:29:30 +0100

    feat: implement login functionality with user authentication

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -41,12 +41,25 @@ const registerUser = asyncHandler(async (req, res) => {
 
   }
 });
+
 // @desc authenticate a user
 // @route POST /api/users/login
 // @access Public
-const loginUser = async (req, res) => {
-  res.json({ message: "Login User" });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {  
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+});
 
 //@desc get user data
 //@route GET /api/users/me
