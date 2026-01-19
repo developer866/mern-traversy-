89d6cd27964ef7261f717deb6acd31cea51aec44
Developer866 <opeyemijoseph866@gmail.com>
2026-01-19 19:55:30 +0100

    feat: implement registration form with state management and UI ADDED README

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import {FaUser} from "react-icons/fa"
 
 function Register() {
+const [formData,setFormData]=useState({
+    name:'',
+    email:'',
+    password:'',
+    password2:'',
+})
+const {name,email,password,password2}=formData
+
   return (
-    <div>Register</div>
+    <div>
+        <section className="heading">
+            <h2>
+                <FaUser/> Register
+            </h2>
+            <p>Please create an account</p>
+        </section>
+       <section className="form">   
+        <form>
+            <input type="text" className="form-control" id="name" name="name" value={name} placeholder="Enter your name" />
+        </form>
+       </section>
+    </div>
   )
 }
 
