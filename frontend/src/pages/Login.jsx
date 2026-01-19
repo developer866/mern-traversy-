239d44d9ae085ce35f70f4960364fce1f72a1e82
Developer866 <opeyemijoseph866@gmail.com>
2026-01-20 00:46:09 +0100

    built Registration and Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import { useState } from 'react'
+import { FaSignInAlt } from "react-icons/fa"
 
 function Login() {
+  const [formData, setFormData] = useState({
+
+    email: '',
+    password: '',
+
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <section className="heading">
+        <h2>
+          <FaSignInAlt /> Login
+        </h2>
+        <p>Login and start Setting Goals</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+
+          <div className="form-group">
+
+            <input type="email" className="form-control"
+              id="email" name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+
+            <input type="password" className="form-control"
+              id="password" name="password"
+              value={password}
+              placeholder="Enter your Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type='submit' className='btn btn-block'>Register</button>
+          </div>
+
+        </form>
+      </section>
+    </div>
   )
 }
 
