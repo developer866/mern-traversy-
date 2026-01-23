6887596e20cad940eb69c5078a069480cd41c9d8
Developer866 <opeyemijoseph866@gmail.com>
2026-01-23 22:04:30 +0100

    fix: correct import paths for GoalForm, GoalItem, and Spinner components

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-// import GoalForm from '../components/GoalForm'
-import GoalItem from '../components/GoalItem'
-import Spinner from '../components/Spinner'
+import GoalForm from '../Components/GoalForm'
+import GoalItem from '../Components/GoalItems'
+import Spinner from '../Components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
